4e4e8909556b33b8135ba261bac75aa750d4f081
Igor Ponomarov <igor@MacBook-Air-de-Igor-2.local>
2025-11-04 19:10:54 +0000

    balance

--- src/MotorController.h
+++ src/MotorController.h
@@ -3,20 +3,19 @@
 
 #include <Arduino.h>
 #include "pico4drive.h"
-//#include "robot.h"
 
 
 class MotorController
 {
     public:
     pico4drive_t pico4drive;
-    //robot_t robot;
+    
     float v_ref, p_ref;
     
     float kp,ki;
-    
+    float prev_Ierro;
 
-    void setW(float w1r,float w2r, float w1e, float w2e);
+    float setWhellW(float w1r,float w2r, float w1e, float w2e);
     float compute_PI(float erro);//gives-me the u
 
     void driveMotor(float u1,float u2);
